5e2ec4c1889a53d0dd001cba34e6aa30b70b1078
Lucas Machado <lucas.fmachado19@gmail.com>
2022-05-17 19:06:26 -0300

    Finalização do desafio

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,18 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+
+    const user = this.usersRepository.findById(user_id)
+
+    if (!user) {
+      throw new Error("mensagem do erro")
+    }
+    
+    if (!user.admin) {
+      throw new Error("mensagem do erro")
+    }
+    
+    return this.usersRepository.list()
   }
 }
 
